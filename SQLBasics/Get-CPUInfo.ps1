a55928ffaca078eeb71adb951c00a197ff76dd27
noobishsre <noobishsre@gmail.com>
2017-04-05 18:35:45 -0500

    Moved to sub dir

---
+++ SQLBasics/Get-CPUInfo.ps1
@@ -0,0 +1,12 @@
+﻿
+
+Function Get-CPUInfo {
+
+    $pcName = Read-Host "Enter Machine Name:"
+    $cpuData = Get-WmiObject Win32_Process -computername $pcName | `
+    Select Name, @{Name="CPU_Time"; Expression={$_.kernelmodetime + $_.usermodetime}} | `
+    sort CPU_Time -Descending | select -first 5
+    return $cpuData
+}
+Get-CPUInfo
+
---
+++ SQLBasics/Insert-NewEmployee.ps1
@@ -0,0 +1,29 @@
+﻿function Insert-NewEmployee{
+
+    $fn = Read-Host "First Name:"
+    $fn = "'$fn'"
+    $ln = Read-Host "Last Name:"
+    $ln = "'$ln'"
+    $empID = Read-Host "Employee ID:"
+    $oID = Read-Host "Office ID:"
+    $city = Read-Host "City:"
+    $city = "'$city'"
+    $state = Read-Host "State:"
+    $state = "'$state'"
+    $zipCode = Read-Host "Zip Code:"
+    $dept = Read-Host "Department:"
+    $jobType = Read-Host "Job Type(code):"
+
+    $insertEmpInfo = "INSERT INTO test_org..emp_info"
+    $insertOrgInfo = "INSERT INTO test_org..org_info"
+    $empValues = "VALUES($fn,$ln,$empID,$dept,$jobType)"
+    $orgValues = "VALUES($oID,$state,$zipCode,$city,$empID,$fn,$ln)"
+
+
+    $qryOne = "$insertEmpInfo $empValues"
+    $qryTwo = "$insertOrgInfo $orgValues"
+
+    Invoke-SQLCMD -query $qryOne -ServerInstance NSRESQL1
+    Invoke-SQLCMD -query $qryTwo -ServerInstance NSRESQL1
+}
+Insert-NewEmployee
