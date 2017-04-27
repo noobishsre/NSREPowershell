22665158798a0a6d553b5877b3a1591042893d5c
noobishsre <noobishsre@gmail.com>
2017-04-26 22:41:48 -0500

    Added Network-Check function

---
+++ Troubleshooting/Network-Check.ps1
@@ -0,0 +1,24 @@
+﻿function Network-Check($pcName)
+{
+    ## Hardcoded servername
+    $server = "Server_Name"
+    
+    ## Get PC Name to test
+    $pcName = Read-Host "PC Name:"
+    
+    ## Ping PC
+    $ping = Test-Connection $pcName -Count 1
+    
+    ## If no response, write message, otherwise, test server connection
+    if($ping -eq $NULL)
+    {
+        Write-Host "Unable to reach computer"
+    }
+    else
+    {
+        $ping | format-table
+        $srvrCheck = Test-Connection $server
+        return $srvrCheck
+    }
+}
+Network-Check
