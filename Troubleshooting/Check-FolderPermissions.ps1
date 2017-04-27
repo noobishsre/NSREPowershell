853283aed0cf96e5a5c90680ebccfc08178b0930
noobishsre <noobishsre@gmail.com>
2017-04-26 23:07:32 -0500

    Check-FolderPermissions and Get-FirefoxVersion

---
+++ Troubleshooting/Check-FolderPermissions.ps1
@@ -0,0 +1,7 @@
+﻿Function Check-FolderPermissions
+{
+    ## Check folder permissions for CCleaner folder
+    $result = (Get-Acl 'C:\Program Files\CCleaner').Access | where{$_.IdentityReference -eq "BUILTIN\Users"}
+    return $result
+}
+Check-FolderPermissions
---
+++ Troubleshooting/Get-FirefoxVersion.ps1
@@ -0,0 +1,12 @@
+﻿Function Get-FirefoxVersion
+{
+    ## Hard coded path of Firefox Version on 64bit system
+    $path = "HKLM\SOFTWARE\Wow6432Node\Mozilla\Mozilla Firefox"
+    
+    ## Query Registry
+    $ffVersion = reg query $path /v CurrentVersion
+    
+    ## Return Version
+    return $ffVersion
+}
+Get-FirefoxVersion
