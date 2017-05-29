7e98e4d27e9ff994eaa7b07962438af68de59516
Derrick E <de@dkfu.com>
2017-05-29 14:29:29 -0500

    Get-AssetInfo: Test Round

--- Troubleshooting/Hardware/Get-AssetInfo.ps1
+++ Troubleshooting/Hardware/Get-AssetInfo.ps1
@@ -1,30 +1,93 @@
-﻿Function Get-AssetInfo{
+﻿Function Get-AssetInfo
+{
+    Param(
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        HelpMessage="Asset Name?")]
+	    [string] $asset
+        
+    )
 
-#############################################
-#TODO
-## Pipeline work for targeting specific data
-## Create object for storing return data
-## Create Baseline validation functionality of return data
-## Add a component or two that act on the data 
+    $logFile = "C:\Temp\AssetInfo-$asset.txt"
+    ##########################################################
+    ## Gather BIOS version and Device Serial Number
+    ##########################################################
+    $winBiosObj = Get-WmiObject -Class Win32_BIOS -ComputerName . | `
+    Select SMBIOSBIOSVersion, SerialNumber | `
+    out-file -append $logFile
 
-Get-WmiObject -Class Win32_BIOS -ComputerName .
+    ##########################################################
+    ## Gather system information
+    ##########################################################
+    $CompSysObj = Get-WmiObject -Class Win32_ComputerSystem | `
+    Select-Object -Property PSComputerName, BootupState, Status, Name, `
+    DNSHostname, Domain, Manufacturer, Model, SystemType, UserName | `
+    out-file -append $logFile
 
-Get-WmiObject -Class Win32_Processor -ComputerName . | Select-Object -Property [a-z]*
+    ##########################################################
+    ## Check installed hotfixes
+    ##########################################################
+    $hotFixObj = Get-WmiObject -Class Win32_QuickFixEngineering -ComputerName . | `
+    Select-Object -Property HotFixID, InstalledOn, InstalledBy | `
+    out-file -append $logFile
 
-Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property [a-z]*
+    ##########################################################
+    ## Get Operating System Information
+    ##########################################################
+    $winOSObj = Get-WmiObject -Class Win32_OperatingSystem -ComputerName . | `
+    Select-Object -Property BuildNumber,BuildType,OSType,ServicePackMajorVersion,ServicePackMinorVersion | `
+    out-file -append $logFile
 
-Get-WmiObject -Class Win32_QuickFixEngineering -ComputerName . | Select-Object -Property [a-z]*
+    ##########################################################
+    ## Get HDD Information
+    ##########################################################
+    $winDiskObj = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType=3" -ComputerName . | `
+    Select DeviceID, @{n="Size";e={[math]::Round($_.Size/1GB,2)}},@{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}} | `
+    out-file -append $logFile
 
-Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType=3" -ComputerName .
+    ##########################################################
+    ## Top 5 processes utilizing CPU
+    ##########################################################
+    $cpuStatus = Get-CPUStatus $asset | out-file -append $logFile
 
-Get-WmiObject -Class Win32_ComputerSystem -Property * -ComputerName .
+    ##########################################################
+    ## Service checks and restarts if necessary
+    ##########################################################
+    $serviceList = @("NetLogon", "DNS", "Spooler", "sqlwriter")
 
-Get-WmiObject -Class Win32_LocalTime -ComputerName . | Select-Object -Property [a-z]*
+    foreach($p in $serviceList)
+    {
+        
+        $serv = Get-Service -ComputerName . | where-object {$_.Name -eq $s}
+        $serv | select Name, Status | format-table | out-file -append $logFile
+        if($serv.Status -eq "Stopped")
+        {
+            Write-Host "$status has been found in a non running state, attempting to restart" | out-file -append $logFile
+            try
+            {
+                start-service $s
+            }
+            catch
+            {
+                ##Error
+            }
 
-Get-WmiObject -Class Win32_Service -ComputerName . | where-object {$_.DisplayName -match "Hyper"} | Format-Table -Property Status,Name,DisplayName -AutoSize -Wrap
+            get-service $s
+            if($s.Status -eq "Stopped")
+            {
+                Write-Host "Unable to restart $s service" | out-file -append $logFile
+            }
+        }
+    }
 
-Get-WmiObject Win32_PNPEntity | Where-Object{$_.ConfigManagerErrorCode -ne 0} | Select Name, DeviceID
 
-Get-WmiObject win32_networkadapterconfiguration | select Description, DHCPEnabled, IPEnabled, ServiceName #could also add Index
+    ##########################################################
+    ## Future Use
+    ##########################################################
+    <#
+    Get-WmiObject Win32_PNPEntity | Where-Object{$_.ConfigManagerErrorCode -ne 0} | Select Name, DeviceID
+
+    Get-WmiObject win32_networkadapterconfiguration | select Description, DHCPEnabled, IPEnabled, ServiceName #could also add Index
+    #>
 }
 export-modulemember -function Get-AssetInfo
