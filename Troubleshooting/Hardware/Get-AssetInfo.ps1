79355df211426ef42407f7565303ffe4dbf5503b
Derrick E <de@dkfu.com>
2017-05-29 14:35:25 -0500

    updated get-assetinfo

--- Troubleshooting/Hardware/Get-AssetInfo.ps1
+++ Troubleshooting/Hardware/Get-AssetInfo.ps1
@@ -59,7 +59,7 @@
     {
         
         $serv = Get-Service -ComputerName $asset | where-object {$_.Name -eq $s}
-        $serv | select Name, Status | format-table | out-file -append $logFile
+        $serv | select Name, Status | out-file -append $logFile
         if($serv.Status -eq "Stopped")
         {
             "$status has been found in a non running state, attempting to restart" | out-file -append $logFile
