a7b9c4bfbc986f00ff46ea61b7fe0f443096179f
noobishsre <noobishsre@gmail.com>
2017-06-24 13:01:07 -0500

    Updated Get-DeviceSecurityStatus

--- Troubleshooting/Security/Get-DeviceSecurityStatus.ps1
+++ Troubleshooting/Security/Get-DeviceSecurityStatus.ps1
@@ -17,10 +17,30 @@ Function Get-DeviceSecurityStatus
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true,
         HelpMessage="Device Name?")]
-	    [string] $pc
-        
+	    [string] $pc,
+        [Parameter(Mandatory=$true)]
+        [String]$usr
     )
 
+    #####################################################
+    ## Check hotfix installation status
+    #####################################################
     Get-HotFix -Description "Security*" -ComputerName "Server01" #-Cred "Server01\admin01"
+
+    <# Other Examples
+
+    $A = Get-Content "servers.txt"
+    $A | ForEach { if (!(Get-HotFix -Id "KB957095" -ComputerName $_)) { Add-Content $_ -Path "Missing-kb953631.txt" }}
+
+    (Get-HotFix | sort installedon)[-1]
+    #>
+
+    #####################################################
+    ## Check last time local admin password was reset
+    #####################################################
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement 
+    $PrincipalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine, $pc)
+    $User = [System.DirectoryServices.AccountManagement.UserPrincipal]::FindByIdentity($PrincipalContext, $usr)
+    $User.LastPasswordSet
 }
 Export-ModuleMember -Function Get-DeviceSecurityStatus
